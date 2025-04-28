1fd7773fd82eae47cb9d4d4fb219ce30d41246cb
Kuzma <ivankuzmanovic87@gmail.com>
2025-04-28 17:00:16 +0200

    signature added

---
+++ utils/project-info.ts
@@ -0,0 +1,12 @@
+/**
+ * Project Metadata - Easter Egg File
+ * Created by: Ivan Kuzmanovic
+ */
+
+export const PROJECT = {
+    name: "MTS Playwright Automation",
+    author: "Ivan Kuzmanovic 🤙",
+    email: "ivankuzmanovic87@gmail.com",
+    builtWith: "Playwright + TypeScript",
+    alias: "QzmA",
+};
